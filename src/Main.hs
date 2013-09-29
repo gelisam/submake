504dc961938dd25368c2eb876131ec9a7ec2ce2a
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-29 16:53:14 -0400

    cp passes

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,29 @@
 module Main where
 
+import Control.Applicative
 import System.IO
 import System.IO.Error
 import System.IO.Error.Extra
+
+
+data Recipe = Recipe { sourceFile :: String
+                     , targetFile :: String
+                     }
+  deriving (Eq, Show)
+
+parseRecipe :: String -> Recipe
+parseRecipe xs = Recipe s t where
+  (t, xs') = break (== ':') xs
+  s = dropWhile (`elem` ": ") xs'
+
+parseSubmakefile :: String -> [Recipe]
+parseSubmakefile = map parseRecipe . lines
+
+
+cook :: Recipe -> IO ()
+cook r = withFile (targetFile r) WriteMode $ \t ->
+         withFile (sourceFile r) ReadMode $ \s ->
+           hPutStr t =<< hGetContents s
 
 
 withSubmakefile :: (Handle -> IO ()) -> IO ()
@@ -12,4 +33,5 @@ withSubmakefile = tryWithFile catch "submakefile" ReadMode
               error "no submakefile found."
 
 main = withSubmakefile $ \f -> do
-         return ()
+         xs <- hGetContents f
+         mapM_ cook $ parseSubmakefile xs
