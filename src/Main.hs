99419fd95c5f5087e4813b99ec456aa46930462a
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-29 16:01:48 -0400

    refactor error functions

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,8 @@
 module Main where
 
-import Control.Exception (bracket)
-import Data.Functor
-import Data.List
 import System.IO
 import System.IO.Error
-
-
--- catches or re-raises the exception.
-catchCond :: [(IOError -> Bool, IOError -> IO a)] -> IOError -> IO a
-catchCond cases e = case find (($ e) . fst) cases of
-                      Just (_, catch) -> catch e
-                      Nothing         -> ioError e
-
-catchWhen :: (IOError -> Bool) -> (IOError -> IO a) -> IOError -> IO a
-catchWhen p catch = catchCond [(p, catch)]
-
--- only catch exceptions caused by the openFile
--- (as opposed to the entire body)
-tryWithFile :: (IOError -> IO a)
-            -> FilePath -> IOMode -> (Handle -> IO a) -> IO a
-tryWithFile catch f m body = do
-    r <- tryIOError $ openFile f m
-    case r of
-      Left e  -> catch e
-      Right h -> body h
+import System.IO.Error.Extra
 
 
 withSubmakefile :: (Handle -> IO ()) -> IO ()
