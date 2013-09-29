235ba8f8a3e96fe789416d0df3149a1320fbf051
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-29 14:39:41 -0400

    empty-submakefile passes

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,37 @@
 module Main where
 
+import Control.Exception (bracket)
+import Data.Functor
+import Data.List
 import System.IO
+import System.IO.Error
 
 
-main = error "no submakefile found."
+-- catches or re-raises the exception.
+catchCond :: [(IOError -> Bool, IOError -> IO a)] -> IOError -> IO a
+catchCond cases e = case find (($ e) . fst) cases of
+                      Just (_, catch) -> catch e
+                      Nothing         -> ioError e
+
+catchWhen :: (IOError -> Bool) -> (IOError -> IO a) -> IOError -> IO a
+catchWhen p catch = catchCond [(p, catch)]
+
+-- only catch exceptions caused by the openFile
+-- (as opposed to the entire body)
+tryWithFile :: (IOError -> IO a)
+            -> FilePath -> IOMode -> (Handle -> IO a) -> IO a
+tryWithFile catch f m body = do
+    r <- tryIOError $ openFile f m
+    case r of
+      Left e  -> catch e
+      Right h -> body h
+
+
+withSubmakefile :: (Handle -> IO ()) -> IO ()
+withSubmakefile = tryWithFile catch "submakefile" ReadMode
+  where
+    catch = catchWhen isDoesNotExistError $ \_ ->
+              error "no submakefile found"
+
+main = withSubmakefile $ \f -> do
+         return ()
