81035822ae7c0c15d9e574399605de26f98f12fd
Samuel Gélineau <gelisam+github@gmail.com>
2013-09-29 22:05:42 -0400

    multi-step passes

--- src/Submake/Cooker.hs
+++ src/Submake/Cooker.hs
@@ -10,6 +10,7 @@ import Submake.Types
 shell_io :: String -> Handle -> IO Handle
 shell_io cmd input = do
     (Nothing, Just output, Nothing, _) <- createProcess info'
+    hSetBinaryMode output True
     return output
   where
     info = shell cmd
@@ -21,5 +22,7 @@ shell_io cmd input = do
 cook :: Recipe -> IO ()
 cook r = withFile (targetFile r) WriteMode $ \t ->
          withFile (sourceFile r) ReadMode $ \s -> do
+           hSetBinaryMode t True
+           hSetBinaryMode s True
            output <- foldM (flip shell_io) s (commands r)
            hPutStr t =<< hGetContents output
